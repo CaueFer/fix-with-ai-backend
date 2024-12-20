85528a107328f19c546e32e42edaf69f734bd5d2
cauefer <cauefernandescaetano@gmail.com>
2024-12-20 19:47:18 -0300

    UPDATE: improve prompt

--- app/controllers/ia_chat_controller.py
+++ app/controllers/ia_chat_controller.py
@@ -18,7 +18,7 @@ def summarizeWithLllama(text):
     chat_history = []
     
     prompt = f"""
-    Retorne o conteúdo no formato JSX semântico, utilizando apenas as tags de texto adequadas, como <h1>, <h2>, <h3>, <p>, <ul>, <li>, entre outras, e adicione **somente** as classes do Tailwind CSS para estilização de texto, como `text-sm`, `text-lg`, `md:text-xl`, `text-color`, `margins`, `font-weight`, ou outras classes relacionadas ao estilo do texto, incluindo as variações de tamanho responsivo (como `md:text-lg`, `lg:text-xl`, etc.). Não adicione explicações, descrição do formato ou links `<a>`. O conteúdo deve ser um resumo em português dos tópicos mais importantes do seguinte texto. Ao final, aguarde minha solicitação para explicar algum tópico específico.
+    Retorne o conteúdo no formato JSX semântico, utilizando apenas as tags de texto adequadas, como <h1>, <h2>, <ul>, <li>,<p> entre outras. Organize o conteúdo com um título principal (<h1>), um subtítulo (<h2>) e, em seguida, os tópicos em uma lista (<ul>) com os itens principais como <li>. Não adicione explicações, descrição do formato ou links `<a>`. O conteúdo deve ser um resumo em português dos tópicos mais importantes do seguinte texto. Ao final, aguarde minha solicitação para explicar algum tópico específico.
 
     Conteúdo:
     {text}
